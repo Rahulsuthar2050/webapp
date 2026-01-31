ba4f2ffbeadbf2d75d31e5d4f3d6fd01934f774d
Rahulsuthar2050 <130124851+Rahulsuthar2050@users.noreply.github.com>
2026-01-31 22:53:34 +0530

    datbase added dockerfile added and auentication is working

--- python/main.py
+++ python/main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import os
+import psycopg2
 sd = os.path.abspath('./HTML')
 HTML_dir = os.path.abspath('./HTML/HTMLS')
 app = Flask(__name__,static_folder=sd,template_folder=HTML_dir)
@@ -8,8 +9,31 @@ app = Flask(__name__,static_folder=sd,template_folder=HTML_dir)
 def index():
     return render_template('index.html')
 
-@app.route('/login')
+def getdbconn():
+    conn=psycopg2.connect(host="localhost",database="webapp",user="rahul",password="rahul9731162684",port="5432")
+    return conn
+
+@app.route('/login_page',methods=['GET', 'POST'])
 def login_page():
+    if request.method == 'POST':
+        username_input= request.form.get('uname')
+        password_input= request.form.get('psw')
+        conn = getdbconn()
+        cur=conn.cursor()
+
+        cur.execute("SELECT name,password from users where name = %s", (username_input,))
+        user = cur.fetchone()
+        cur.close()
+        conn.close()
+        if user:
+            stored_n =user[0]
+            stored_p = user[1]
+            if password_input == stored_p:
+                return f"Welcome {stored_n} verfication successfull!"
+            else:
+                return f"invalid username or pass"
+        else:
+            return f"User not found"
     return render_template('login.html')
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000,debug=True)
