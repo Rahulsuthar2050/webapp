7945e6ac5947e335db67885b64db12e9e120432f
Rahulsuthar2050 <130124851+Rahulsuthar2050@users.noreply.github.com>
2026-06-03 22:00:47 +0530

    Create main1.py

---
+++ python/main1.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, request
+import os
+import psycopg2
+from dotenv import load_dotenv
+load_dotenv()
+dbuser="postgress"
+dbpass="Rahul@123"
+dbhost="123"
+dbport="5432"
+sd = os.path.abspath('./HTML')
+HTML_dir = os.path.abspath('./HTML/HTMLS')
+app = Flask(__name__,static_folder=sd,template_folder=HTML_dir)
+@app.route('/health')
+def health_check():
+    return "OK", 200
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def getdbconn():
+    conn=psycopg2.connect(host=dbhost,database="webapp",user=dbuser,password=dbpass,port=dbport)
+    return conn
+
+@app.route('/login_page',methods=['GET', 'POST'])
+def login_page():
+    if request.method == 'POST':
+        username_input= request.form.get('uname')
+        password_input= request.form.get('psw')
+        conn = getdbconn()
+        cur=conn.cursor()
+
+        cur.execute("SELECT name,password from users where name = %s", (username_input,))
+        user = cur.fetchone()
+        cur.close()
+        conn.close()
+        if user:
+            stored_n =user[0]
+            stored_p = user[1]
+            if password_input == stored_p:
+                return f"Welcome {stored_n} verfication successfull!"
+            else:
+                return f"invalid username or pass"
+        else:
+            return f"User not found"
+    return render_template('login.html')
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5000,debug=True)
